f5ae1c27f1c755ca0949062c892bd883316ff2ac
hossein <hossein.yousefi23@gmail.com>
2017-07-08 13:13:20 +0430

    changed static root

--- lilipop/settings.py
+++ lilipop/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = "/var/www/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
